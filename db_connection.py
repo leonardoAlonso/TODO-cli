2fba7bbc2870224db002753b58a53aa5a8497274
Leonardo Alonso <leonardoalonsososa@gmail.com>
2020-09-19 14:27:58 -0500

    :sparkles: List al TODO list
    - Improve print data using colors and emoji
    - [ADD] Now you can list all TODO created lists

--- db_connection.py
+++ db_connection.py
@@ -1,9 +1,13 @@
+#!usr/bin/env python
+
+from datetime import datetime
 import peewee
 
 database = peewee.SqliteDatabase('todoCLI.db')
 
 class List(peewee.Model):
     list_name = peewee.CharField(max_length=50, unique=True, null=True)
+    creation_date = peewee.DateField(default=datetime.now())
 
     def __str__(self):
         return self.list_name
@@ -13,12 +17,12 @@ class List(peewee.Model):
         db_table = 'list'
 
 class Task(peewee.Model):
-    list_id = peewee.ForeignKeyField(List, backref='list')
+    list_id = peewee.ForeignKeyField(List, backref='task')
     desc = peewee.CharField(max_length=200, null=False)
     state = peewee.BooleanField(default=False)
 
     def __str__(self):
-        return '{} task for {} list'.format(desc, list_id)
+        return '{} task for {} list'.format(self.desc, self.list_id)
 
     class Meta:
         database = database # this model uses todoCLI.db
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 #!usr/bin/env python
-import click
+
+from tabulate import tabulate
+from datetime import datetime
 from db_connection import database, List, Task
+from colors import bcolors
+import click
 import peewee
 
 MODELS = [ List, Task ]
@@ -14,19 +18,31 @@ def init():
     """Creates the tables and the database connection"""
     with database:
         database.create_tables(MODELS)
+    print(bcolors.HEADER + 'TODO-cli ready to be useed')
+    print("Use --help to get all possible commands")
 
 @main.command()
 @click.argument('list_name')
 def create_list(list_name):
     """Create a new list"""
-    print("Estas a punto de crear la lista: ", list_name)
-    confirm = input("Deseas continuar (Y/N): ")
-    if confirm == "Y":
+    print(bcolors.WARNING + 'You are about to create: ' +
+          bcolors.OKBLUE + list_name)
+    confirm = input(bcolors.WARNING + 'Continue? \U0001F914 (Y/N): ')
+    if confirm != "N":
         try:
-            lista = List.create(list_name=list_name)
-            print("Lista creada :)")
-        except peewee.IntegrityError as e:
-            print("Upps ya existe una lista con ese nombre :(")
+            List.create(list_name=list_name)
+            print(bcolors.OKGREEN + "Success \U0001f44d")
+        except peewee.IntegrityError:
+            print(bcolors.FAIL + 'You already has a list with this name \U0001F616')
+
+@main.command()
+def get_all_list():
+    """Get all list names"""
+    for l in List.select():
+        print(tabulate([[bcolors.OKGREEN + str(l.id),
+                         bcolors.OKGREEN + l.list_name,
+                         bcolors.OKGREEN + l.creation_date.strftime('%m/%d/%Y')]],
+                         headers=[bcolors.HEADER + 'ID', bcolors.HEADER + 'Name', bcolors.HEADER + 'Creation date']))
 
 if __name__ == '__main__':
     main()
