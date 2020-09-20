ce7e346b2445afe3415f109ad2797e717ea7ce6f
Leonardo Alonso <leonardoalonsososa@gmail.com>
2020-09-20 12:25:28 -0500

    :white_check_mark: Test Database creation

---
+++ test/test_common.py
@@ -0,0 +1,20 @@
+#!usr/bin/env python
+
+import unittest
+import peewee
+
+from database_connection.db_connection import List, Task
+
+MODELS = [List, Task]
+
+test_db = peewee.SqliteDatabase(':memory:')
+
+
+class TestCommon(unittest.TestCase):
+
+    def setUp(self):
+        test_db.connect()
+        test_db.create_tables(MODELS)
+
+    def tearDown(self):
+        test_db.close()
---
+++ test/test_db.py
@@ -0,0 +1,19 @@
+#!usr/bin/env python
+
+import unittest
+import peewee
+from test_common import TestCommon
+from database_connection.db_connection import List, Task
+
+
+class TestTables(TestCommon):
+
+    def test_check_list_exist(self):
+        assert List.table_exists()
+
+    def test_check_task_exist(self):
+        assert Task.table_exists()
+
+
+if __name__ == '__main__':
+    unittest.main()
