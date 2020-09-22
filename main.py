f625afcdb09c1f6861cbe3bcc046bdd54ef7ff18
Leonardo Alonso <leonardoalonsososa@gmail.com>
2020-09-22 07:49:40 -0500

    :recycle: [FIX] Using click echo insted of print
    Now we use the echo feature of click library, and the click style to add colors instead of the custom colors based on unicode format

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 
 from tabulate import tabulate
 from database_connection.db_connection import database, List, Task
-from colors import bcolors
 import click
 import peewee
 
@@ -19,45 +18,47 @@ def init():
     """Creates the tables and the database connection"""
     with database:
         database.create_tables(MODELS)
-    print(bcolors.OKGREEN + 'TODO-cli ready to be useed')
-    print("Use --help to get all possible commands")
+    click.echo(click.style('Success! \nTODO-cli ready to be useed', fg='green'))
+    click.echo("Use --help to get all possible commands")
 
 
 @main.command()
 @click.argument('list_name')
 def create_list(list_name):
     """Create a new list"""
-    print(bcolors.WARNING + 'You are about to create: ' +
-          bcolors.OKBLUE + list_name)
-    confirm = input(bcolors.WARNING + 'Continue? \U0001F914 (Y/N): ')
-    if confirm != "N":
+    click.echo(click.style('You are about to create: ' + list_name, fg='blue'))
+    if click.confirm(click.style("Continue? \U0001F914", fg='yellow'), abort=True):
         try:
             List.create(list_name=list_name)
-            print(bcolors.OKGREEN + "Success \U0001f44d")
+            click.echo(click.style("Success!", fg='green'))
         except peewee.IntegrityError:
-            print(bcolors.FAIL + 'You already has a list with this name \U0001F616')
+            click.echo(click.style('You already has a list with this name \U0001F616', fg='red'))
 
 
 @main.command()
 def get_all_list():
     """Get all list names"""
     for _list in List.select():
-        print(tabulate([[bcolors.OKGREEN + str(_list.id),
-                         bcolors.OKGREEN + _list.list_name,
-                         bcolors.OKGREEN + _list.creation_date.strftime('%m/%d/%Y')]],
-                       headers=[bcolors.HEADER + 'ID', bcolors.HEADER + 'Name', bcolors.HEADER + 'Creation date']))
+        click.echo(tabulate([[click.style(str(_list.id), fg='green'),
+                              click.style(_list.list_name, fg='green'),
+                              click.style(_list.creation_date.strftime('%m/%d/%Y'), fg='green')]],
+                            headers=[click.style('ID', fg='red'),
+                                     click.style('Name', fg='red'),
+                                     click.style('Creation date', fg='red')]))
 
 
 @main.command()
 @click.argument('list_name')
 def delete_list(list_name):
     """Allows to delete a List based on list name.
-    This will delete all task related /U0001F6A8"""
+    This will delete all task related
+        - argument list_name
+    """
     _list = List.get_or_none(List.list_name == list_name)
     if not _list:
-        print(bcolors.FAIL + "This list does not exist \U0001F625")
+        click.echo(click.style("This list does not exist \U0001F625"), fg='red')
     _list.delete().execute()
-    print(bcolors.OKGREEN + "The list has been deleted \U0001F5D1")
+    click.echo(click.style("The list has been deleted \U0001F5D1", fg='green'))
 
 
 if __name__ == '__main__':
