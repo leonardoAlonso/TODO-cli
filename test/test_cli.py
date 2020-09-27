1ee2747068d00c306b446be7d675c91b000dd802
Leonardo Alonso <leonardoalonsososa@gmail.com>
2020-09-26 20:55:56 -0500

    :white_check_mark: Test all creation and deletion List methods

--- test/test_cli.py
+++ test/test_cli.py
@@ -12,6 +12,33 @@ class TestTables(TestCommon):
         runner = CliRunner()
         result = runner.invoke(main, ['create-list', 'lista'], input='y')
         assert result.exit_code == 0
+        assert 'Success' in result.output
+
+    def test_create_list_same_name(self):
+        runner = CliRunner()
+        list_one = runner.invoke(main, ['create-list', 'lista'], input='y')
+        assert list_one.exit_code == 0
+        assert 'Success' in list_one.output
+        list_two = runner.invoke(main, ['create-list', 'lista'], input='y')
+        assert 'already has a list with this name' in list_two.output
+
+    def test_delete_list(self):
+        runner = CliRunner()
+        result = runner.invoke(main, ['create-list', 'lista'], input='y')
+        assert result.exit_code == 0
+        assert 'Success' in result.output
+        delete = runner.invoke(main, ['delete-list', 'lista'])
+        assert delete.exit_code == 0
+        assert 'deleted' in delete.output
+
+    def test_delete_not_exist_list(self):
+        runner = CliRunner()
+        result = runner.invoke(main, ['create-list', 'lista'], input='y')
+        assert result.exit_code == 0
+        assert 'Success' in result.output
+        delete = runner.invoke(main, ['delete-list', 'lista2'])
+        assert delete.exit_code == 0
+        assert 'This list does not exist' in delete.output
 
 
 if __name__ == '__main__':
